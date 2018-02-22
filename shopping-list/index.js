a8a697a0f4a95c3c2967c1b745fa21ccd74b91a5
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-02-22 15:23:47 -0800

    added shopping list

---
+++ shopping-list/index.js
@@ -0,0 +1,14 @@
+//event listener for user click
+//add item to shopping list
+//toggle shopping item
+//remove shopping item from list
+
+$(function()) {
+    $('#js-shopping-list-form').submit(function(event) {
+       event.preventDefault();
+
+       const shoppingItem = $(event.currentTarget).find('.js-shopping-list-entry')
+;
+
+    }
+}
